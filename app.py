14b9586d7bb541afc6bb0532e2f0ff4223a42e51
Ron Vallejo <134008768+ronvallejo@users.noreply.github.com>
2024-08-15 13:51:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ home_page = st.Page("main.py", title="Home", icon=":material/home:", default=Tru
 open_source_chatbot = st.Page("Playground/AtreyuChat.py", title="AtreyuChat", icon="🤖")
 learn_overview = st.Page("Learn/overview.py", title="Introduction to AI", icon="📘")
 learn_demystifying_ai = st.Page("Learn/demystifying_ai.py", title="Demystifying AI", icon="🤔")
-#learn_agents = st.Page("Learn/agents.py", title="AI Agents", icon="🕵️‍♂️")
+learn_models = st.Page("Learn/Model_Cards.py", title="Model Cards", icon="🤖")
 #learn_future_of_ai = st.Page("Learn/future_of_ai.py", title="The Future of AI", icon="🔮")
 news_ai = st.Page("under_construction.py", title="Coming Soon", icon="📰")
 
@@ -14,7 +14,7 @@ pg = st.navigation(
     {
         "Home": [home_page],
         "Playground": [open_source_chatbot],
-        "Learn": [learn_overview, learn_demystifying_ai],
+        "Learn": [learn_overview, learn_demystifying_ai, learn_models],
         "News": [news_ai],
     }
 )
