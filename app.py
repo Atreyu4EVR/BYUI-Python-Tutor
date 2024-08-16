1213eda3af32a22fda7c4b8d4f9c546933820176
Ron Vallejo <134008768+ronvallejo@users.noreply.github.com>
2024-08-15 20:07:50 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,10 @@ learn_models = st.Page("Learn/Model_Cards.py", title="Model Cards", icon="🤖")
 #learn_future_of_ai = st.Page("Learn/future_of_ai.py", title="The Future of AI", icon="🔮")
 news_ai = st.Page("under_construction.py", title="Coming Soon", icon="📰")
 
-st.logo(images/small_Logo.png, icon_image=images/full_logo.png)
+logo = "images/small_Logo.png"
+icon = "images/robot.png"
+
+st.logo(logo)
 
 # Build the navigation
 pg = st.navigation(
