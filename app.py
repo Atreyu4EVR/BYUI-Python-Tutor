3c811b62e21beaaea7f6e98f9e3ab7e89e0a3a04
Ron Vallejo <134008768+ronvallejo@users.noreply.github.com>
2024-08-16 13:52:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
 # Define pages
-home_page = st.Page("main.py", title="Home", icon=":material/home:", default=True)
-open_source_chatbot = st.Page("Playground/AtreyuChat.py", title="AtreyuChat", icon="🤖")
-ai_websearch = st.Page("Playground/AtreyuSearch.py", title="AtreyuSearch", icon="🌐")
-learn_overview = st.Page("Learn/overview.py", title="Introduction to AI", icon="📘")
-learn_demystifying_ai = st.Page("Learn/demystifying_ai.py", title="Demystifying AI", icon="🤔")
-learn_models = st.Page("Learn/Model_Cards.py", title="Model Cards", icon="🤖")
+home_page = st.Page("main.py", title="Home", icon=":material/menu:", default=True)
+open_source_chatbot = st.Page("Playground/AtreyuChat.py", title="AtreyuChat", icon=":material/smart_toy:")
+ai_websearch = st.Page("Playground/AtreyuSearch.py", title="AtreyuSearch", icon=":material/travel_explore:")
+learn_overview = st.Page("Learn/overview.py", title="Introduction to AI", icon=":material/domain_verification:")
+learn_demystifying_ai = st.Page("Learn/demystifying_ai.py", title="Demystifying AI", icon=":material/domain_verification:")
+learn_models = st.Page("Learn/Model_Cards.py", title="Model Cards", icon="web")
 #learn_future_of_ai = st.Page("Learn/future_of_ai.py", title="The Future of AI", icon="🔮")
-news_ai = st.Page("under_construction.py", title="Coming Soon", icon="📰")
+news_ai = st.Page("under_construction.py", title="Coming Soon", icon=":material/devices_off:")
 
 logo = "images/small_Logo.png"
 icon = "images/robot_logo.png"
