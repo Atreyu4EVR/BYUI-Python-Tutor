a43a677bb7d1c335faaf44e1c6cd0c3f38fcc579
Ron Vallejo <134008768+ronvallejo@users.noreply.github.com>
2024-08-17 14:48:13 -0600

    Update AtreyuChat.py

--- Playground/AtreyuChat.py
+++ Playground/AtreyuChat.py
@@ -23,7 +23,7 @@ mountain_offset = timedelta(hours=-6)
 mountain_tz = timezone(mountain_offset, name="MDT")
 utc_now = datetime.now(timezone.utc)
 mountain_time = utc_now.astimezone(mountain_tz)
-formatted_mountain_time = mountain_time.strftime("%m-%d-%Y- %H:%M:%S")
+formatted_mountain_time = mountain_time.strftime("%m-%d-%Y %H:%M:%S MDT")
 current_date_time = ("Current Date/Time:", formatted_mountain_time, "(MDT)")
 
 st.sidebar.text(formatted_mountain_time)
