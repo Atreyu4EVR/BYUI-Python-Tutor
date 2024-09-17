4ebbe6e8b127a470ef15c9f17121aa7a2cf7af4e
Ron Vallejo <134008768+Atreyu4EVR@users.noreply.github.com>
2024-09-16 20:30:35 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,29 +7,9 @@ st.balloons()
 st.header("Introduction")
 
 st.markdown("""
-Atreyu.AI is a platform dedicated to showcasing the capabilities and advancements in AI with the mission is to provide users with an accessible and engaging way to explore and interact with state-of-the-art AI models. Whether you're here to learn about AI, stay updated on the latest technological advancements, or experience the power of AI through our chatbot, Atreyu.AI is designed with you in mind. This site is free to use and doesn't require any information or data from you. We don't store or collect any of your data, in fact all site traffic is anonymous and secured over "HTTPS" making everything more secure from eavesdropping, tampering, and man-in-the-middle attacks. Simply explore the platform and go tour the various tools that are provided securely for your benefit. 
-""")
+Atreyu.AI is web application written entirely in Python using the Streamlit framework. This application is dedicated to showcasing the capabilities of Python for students in CSE 110. Atreyu, the AI Assistant, is a custom AI model designed specifically for the students and is tailor-made to answer questions about programming, the basics of Python, and other course related topics. 
 
-st.subheader("Getting Started")
-
-st.markdown("""
-
-**Explore the Platform:** Whether you’re looking to chat with an AI, read the latest news, or dive into educational content, we’ve made it easy for you to get started.
-
-- **Playground**: At the heart of Atreyu.AI is a free, open-source conversational AI chatbot, powered by an open-source models like Meta's Llama 3. This allows users to engage in natural, human-like conversations with an AI that continuously evolves and improves. Soon, we will be introducing a second chatbot featuring closed-source models from industry leaders like OpenAI and Anthropic, providing even more robust and diverse interactions.
-
-- **Learn**: This section offers a few blog articles tailored to new users of AI. Here, you'll find clear and concise explanations of key AI and machine learning concepts, designed to help you understand the technology that powers modern AI systems.
-
-- **News**: The "News" section is your go-to source for the latest developments in AI and technology. We curate headlines and stories that highlight important breakthroughs, ethical considerations, and industry trends, ensuring you stay informed in a rapidly changing field.
-
-### Vision
-
-Artificial intelligence is here to stay and it's going to soon usher a new industrial revolution. AI will have a disruptive impact on all of our lives for better and in many ways for the worse. It's my belief that everyone should at least have some basic understanding of this new technology in order to be informed and empowered with optimism and hope. This platform aims to demonstrate the capabilities of current AI technologies and increase accessiblity to anyone with an open-mind. Having said that, I'm committed to fostering a deeper understanding of AI, promoting ethical practices, and showcasing the advancements in AI and how it can be leveraged safely and responsibly.
-
-Thank you for visiting Atreyu.AI. We invite you to explore, learn, and engage with the future of AI.
-""")
-
-st.markdown(""" 
+**Playground**: At the heart of Atreyu.AI is a free, open-source conversational AI chatbot, powered by an open-source models like Meta's Llama 3. This allows students to engage in natural, human-like conversations with an AI that continuously evolves and improves.
 """)
 
 # Disclaimer
