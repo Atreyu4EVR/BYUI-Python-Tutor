6cd88f4d76c713daf9ebff0cd0127f25d581fb24
Ron Vallejo <134008768+Atreyu4EVR@users.noreply.github.com>
2024-09-16 20:58:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import streamlit as st
 
 # Define pages
 home_page = st.Page("main.py", title="Home", icon=":material/menu:", default=True)
-open_source_chatbot = st.Page("Playground/AtreyuChat.py", title="AtreyuChat", icon=":material/smart_toy:")
-ai_websearch = st.Page("Playground/AtreyuSearch.py", title="AtreyuSearch", icon=":material/travel_explore:")
-learn_overview = st.Page("Learn/overview.py", title="Introduction to AI", icon=":material/domain_verification:")
-learn_demystifying_ai = st.Page("Learn/demystifying_ai.py", title="Demystifying AI", icon=":material/domain_verification:")
-learn_models = st.Page("Learn/Model_Cards.py", title="Model Cards", icon=":material/web:")
+open_source_chatbot = st.Page("Playground/AtreyuChat.py", title="Python Tutor", icon=":material/smart_toy:")
+#ai_websearch = st.Page("Playground/AtreyuSearch.py", title="AtreyuSearch", icon=":material/travel_explore:")
+#learn_overview = st.Page("Learn/overview.py", title="Introduction to AI", icon=":material/domain_verification:")
+#learn_demystifying_ai = st.Page("Learn/demystifying_ai.py", title="Demystifying AI", icon=":material/domain_verification:")
+#learn_models = st.Page("Learn/Model_Cards.py", title="Model Cards", icon=":material/web:")
 #learn_future_of_ai = st.Page("Learn/future_of_ai.py", title="The Future of AI", icon="🔮")
-news_ai = st.Page("under_construction.py", title="Coming Soon", icon=":material/devices_off:")
+#news_ai = st.Page("under_construction.py", title="Coming Soon", icon=":material/devices_off:")
 
 logo = "images/small_Logo.png"
 icon = "images/robot_logo.png"
@@ -25,9 +25,9 @@ st.sidebar.image("images/small_Logo.png")
 pg = st.navigation(
     {
         "Home": [home_page],
-        "Playground": [open_source_chatbot, ai_websearch],
-        "Learn": [learn_overview, learn_demystifying_ai, learn_models],
-        "News": [news_ai],
+        "Playground": [open_source_chatbot]
+        #"Learn": [learn_overview, learn_demystifying_ai, learn_models],
+        #"News": [news_ai],
     }
 )
 
