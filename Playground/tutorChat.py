4c83add3ec3683a63c6de26504146a70a66b4f7d
Ron Vallejo <134008768+Atreyu4EVR@users.noreply.github.com>
2024-10-30 21:38:12 -0600

    Update tutorChat.py

--- Playground/tutorChat.py
+++ Playground/tutorChat.py
@@ -9,13 +9,12 @@ from datetime import datetime, timezone, timedelta
 LOGO_URL_LARGE = "images/robot_logo.png"
 LOGO_URL_SMALL = "images/robot.png"
 HUMAN_AVATAR = "images/human.png"
-MAX_TOKENS = 4048
+MAX_TOKENS = 2042
 
 user_avatar = "images/human.png"  # replace with your user avatar
 assistant_avatar = "images/robot.png"  # replace with your bot avatar
 
-st.subheader("Python Tutor")
-st.markdown(":violet[made with Meta Llama 3.1]")
+st.subheader("AtreyuChat")
 
 load_dotenv()
 
@@ -29,7 +28,11 @@ current_date_time = ("Current Date/Time:", formatted_mountain_time)
 
 # Supported models
 model_links = {
-    "Meta-Llama 3.1":"meta-llama/Meta-Llama-3.1-8B-Instruct"
+    "Meta-Llama 3.1":"meta-llama/Meta-Llama-3.1-8B-Instruct",
+    "Meta-Llama 3-70B":"meta-llama/Meta-Llama-3-70B-Instruct",
+    "Mistral-Nemo":"mistralai/Mistral-Nemo-Instruct-2407",
+    "Google Gemma 7B":"google/gemma-1.1-7b-it",
+    "Microsoft Phi 3B":"microsoft/Phi-3-mini-4k-instruct"
 }
 
 # Model descriptions and logos
@@ -37,6 +40,22 @@ model_info = {
     "Meta-Llama 3.1": {
         'description': "This is the latest model developed by Meta (Llama3.1 8B). View the [Model Card](https://atreyu.streamlit.app/Model_Cards) for the technical specs.",
         'logo': 'images/llama_logo.gif'
+    },
+    "Meta-Llama 3-70B":{
+        'description': "This is developed by Meta (Llama3 70B). View the [Model Card](https://atreyu.streamlit.app/Model_Cards) for the technical specs.",
+        'logo': 'images/llama_logo.gif'
+    },
+    "Mistral-Nemo": {
+        'description': "This model was developed by Mistral.ai (Mixtral 8x7B). View the [Model Card](https://atreyu.streamlit.app/Model_Cards) for the technical specs.",
+        'logo': 'https://mistral.ai/images/logo_hubc88c4ece131b91c7cb753f40e9e1cc5_2589_256x0_resize_q97_h2_lanczos_3.webp'
+    },
+    "Google Gemma 7B": {
+        'description': "The Zephyr model is a **Large Language Model (LLM)**. View the [Model Card](https://atreyu.streamlit.app/Model_Cards) for the technical specs.",
+        'logo': 'https://huggingface.co/HuggingFaceH4/zephyr-7b-alpha/resolve/main/thumbnail.png'
+    },
+    "Microsoft Phi 3.8B": {
+        'description': "The Phi-3-Mini-4K-Instruct is a 3.8B parameters, lightweight, state-of-the-art open model. View the [Model Card](https://atreyu.streamlit.app/Model_Cards) for the technical specs.",
+        'logo': 'https://cdn-avatars.huggingface.co/v1/production/uploads/1583646260758-5e64858c87403103f9f1055d.png'
     }
 }
 
@@ -44,63 +63,17 @@ models = [key for key in model_links.keys()]
 selected_model = st.sidebar.selectbox("Select Model", models)
 repo_id = model_links[selected_model]  # This is your selected model repo ID
 
-course_description = "The Python Programming course is structured into a comprehensive 14-Week lesson curriculum, covering fundamental concepts and practical applications. Students will use VS Cocde to write and edit their Python code, use version 3.12 of Python, and collaborate with other students, classmates, and their instructor (Ron Vallejo) in Microsoft Teams. Here's a detailed breakdown of the weekly lessons:\n\n1. Introduction to Python and Basic Concepts\nSetting up the Python, VS Code, and getting started with the basics, like Syntax, Strings, Arguments, and Variables. Writing and running simple Python scripts\n\n2. Control Structures and Functions\nConditional statements (if, else, elif)\nLoops (for, while)\nDefining and calling functions\nBasic error handling\n\n3. Data Structures\nLists, tuples, and dictionaries\nString manipulation techniques\nFile input/output operations\n\n4. Object-Oriented Programming\nClasses and objects\nInheritance and polymorphism\nImplementing OOP concepts in Python\n\n5. Modules and Libraries\nImporting and using modules\nExploring popular Python libraries\nException handling techniques\n\n6. Regular Expressions and GUI Programming\nIntroduction to regular expressions\nBasic GUI programming concepts\n\n7-13. Project Development\nSemester-long project work\nWeekly milestones and progress checks\nPeer reviews and instructor feedback\n\n14. Final Project Presentations\nStudents showcase their completed projects\nReflective essay on the development process\n\nThis structured approach ensures a gradual progression from basic concepts to more advanced topics, culminating in a practical project that demonstrates students' acquired skills."
-
-course_schedule = {
-    "Week 1 (Sept 16 - Sept 22, 2024)": "Intro to Python and Basic Concepts Part One",
-    "Week 2 (Sept 23 - Sept 29, 2024)": [
-        "Continued Basic Concepts Part Two",
-        "Control Structures and Functions"
-    ],
-    "Week 3 (Sept 30 - Oct 6, 2024)": "Data Structures",
-    "Week 4 (Oct 7 - Oct 13, 2024)": "Object-Oriented Programming",
-    "Week 5 (Oct 14 - Oct 20, 2024)": "Modules and Libraries",
-    "Week 6 (Oct 21 - Oct 27, 2024)": "Regular Expressions and GUI Programming",
-    "Weeks 7-13 (Oct 28 - Dec 15, 2024)": "Project Development",
-    "Week 14 (Dec 16 - Dec 18, 2024)": "Final Project Presentations"
-}
-
-project_application_ideas = {
-    "Discord Bot",
-    "Simple calculator program",
-    "Number guessing game",
-    "Basic to-do list application",
-    "Health and nutrition tracking application",
-    "Resume generator",
-    "Dice rolling simulator",
-    "Password generator"
-}
-
-info_ricks = {
-    "Brigham Young University-Idaho (BYU-I) was originally known as Ricks College before it transformed from a 2-year to 4-year degree granting university. Ricks College was named after it's original founder, Thomas E. Ricks. More information about the history of BYU-Idaho can be found at: https://www.byui.edu/about/history."
-}    
-
 system_prompt = f"""
-Background: You are Ricks, a tutor for students learning software development in Python. You're named after the founder of the University, Thomas E. Ricks. Your role is to assist college students taking the "CSE 110 Introduction to Programming" at Brigham Young University-Idaho.
-
-Goals:
--Inspire students to use programming to solve everyday problems.
--Encourage personal growth and testimonies in the Gospel and in the Savior Jesus Christ.
-
-Instructions:
-1. Informative and Friendly: Ensure that all information provided is factually correct and uses a conversational tone that is warm and inviting, encouraging users to ask follow-up questions and explore topics regarding Python and programming.
-2. Academic Integrity: Be mindful of the questions asked by students. Your role is to simply aid and guide through the process of solving problems. Your responses reflect a commitment to maintain academic honesty by only providing simple code examples and never the full solution. Aid their learning, not hinder it.
-3. Engaging and Thought-Provoking: Encourage curiosity and deeper inquiry by providing thought-provoking insights and inviting users to explore related topics.
-4. Tone: Be friendly and conversational. It's critical for you to exemplify the wisdom, kindness, and helpfulness of Generative AI tools and assistants like you. For many, you'll be their first impression, so ensure you leave a remarkable impression.
-5. Purpose: to educate, inform, and empower users with knowledge about technology, programming, and Python, helping them to better understand and navigate this rapidly evolving field.
-6. Content: You MUST not answer questions that are not course-related. If asked, politely steer the discussion back to course related topics.
-7. Week Schedule: Ensure that questions and topics are relavant to the date and time relative to the {course_schedule} with the current date ({current_date_time}).
-8. Project: Students will begin working on their final project in Week 2. Use the list project examples of what they can built as their Python application. Here are those ideas: {project_application_ideas}.
-8. About: If needed, information about BYU-Idaho can be found at the school website (www.byui.edu) or our history ({info_ricks}).
+Background: Your name is Atreyu, named after the fearless warrior from the book, Never Ending Story. You’re a helpful chatbot for a young boy named Mason. You're going to assist him with fun ideas, answer his questions, and support him in his daily tasks. You're always there to make his experience enjoyable and positive. Personalized Assistance - chat about his favorite video games like Minecraft, Fortnite, and Rocket League; help with projects or games on his dad's Raspberry Pi 5; talk about his chickens and his dad's duck, Daisy, asking questions about their care and sharing fun facts.
 """
 
 # List of greeting variations
 greetings = [
-    "Hi there! I'm Ricks, your friendly AI tutor. How can I help you with Python today?",
-    "Greetings! I'm Ricks, your go-to AI tutor. Do you have any questions about programming",
-    "Hello! I'm Ricks, your AI tutor. What would you like to know about Python today?",
-    "Hey! I'm Ricks, your AI tutor. Is there anything you'd like to learn about Python",
-    "Hello! I'm Ricks, your AI tutor. What would you like to know about programming?"
+    "Hi there! I'm Atreyu, your friendly AI assistant. How can I help you with Python today?",
+    "Greetings! I'm Atreyu, your go-to AI assistant. Do you have any questions about programming",
+    "Hello! I'm Atreyu, your AI guide. What would you like to know about Python today?",
+    "Hey! I'm Atreyu, your AI assistant. Is there anything you'd like to learn about Python",
+    "Hello! I'm Atreyu, your AI assistant. What would you like to know about programming?"
 ]
 
 # Function to select a random greeting
@@ -161,7 +134,6 @@ def generate_response():
     for event in client.chat_completion(
         messages=[{"role": "user", "content": prompt_str}],
         max_tokens=MAX_TOKENS,
-        temperature=0.5,
         stream=True
     ):
         yield str(event.choices[0].delta.content)
