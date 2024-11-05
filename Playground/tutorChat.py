f5eabf1f9cccc6641f5ba943b6b32abc2f8bb35e
Ron Vallejo <134008768+Atreyu4EVR@users.noreply.github.com>
2024-11-05 16:53:14 -0700

    Update tutorChat.py

--- Playground/tutorChat.py
+++ Playground/tutorChat.py
@@ -14,7 +14,7 @@ MAX_TOKENS = 2042
 user_avatar = "images/human.png"  # replace with your user avatar
 assistant_avatar = "images/robot.png"  # replace with your bot avatar
 
-st.subheader("AtreyuChat")
+st.subheader("BYU-Idaho Python Tutor")
 
 load_dotenv()
 
@@ -25,6 +25,23 @@ utc_now = datetime.now(timezone.utc)
 mountain_time = utc_now.astimezone(mountain_tz)
 formatted_mountain_time = mountain_time.strftime("%m-%d-%Y %H:%M:%S MDT")
 current_date_time = ("Current Date/Time:", formatted_mountain_time)
+
+course_description = "The Python Programming course is structured into a comprehensive 14-Week lesson curriculum, covering fundamental concepts and practical applications. Students will use VS Cocde to write and edit their Python code, use version 3.12 of Python, and collaborate with other students, classmates, and their instructor (Ron Vallejo) in Microsoft Teams. Here's a detailed breakdown of the weekly lessons:\n\n1. Introduction to Python and Basic Concepts\nSetting up the Python, VS Code, and getting started with the basics, like Syntax, Strings, Arguments, and Variables. Writing and running simple Python scripts\n\n2. Control Structures and Functions\nConditional statements (if, else, elif)\nLoops (for, while)\nDefining and calling functions\nBasic error handling\n\n3. Data Structures\nLists, tuples, and dictionaries\nString manipulation techniques\nFile input/output operations\n\n4. Object-Oriented Programming\nClasses and objects\nInheritance and polymorphism\nImplementing OOP concepts in Python\n\n5. Modules and Libraries\nImporting and using modules\nExploring popular Python libraries\nException handling techniques\n\n6. Regular Expressions and GUI Programming\nIntroduction to regular expressions\nBasic GUI programming concepts\n\n7-13. Project Development\nSemester-long project work\nWeekly milestones and progress checks\nPeer reviews and instructor feedback\n\n14. Final Project Presentations\nStudents showcase their completed projects\nReflective essay on the development process\n\nThis structured approach ensures a gradual progression from basic concepts to more advanced topics, culminating in a practical project that demonstrates students' acquired skills."
+
+
+project_application_ideas = {
+    "Discord Bot",
+    "Simple calculator program",
+    "Number guessing game",
+    "Basic to-do list application",
+    "Health and nutrition tracking application",
+    "Resume generator",
+    "Dice rolling simulator",
+    "Password generator"
+}
+info_ricks = {
+    "Brigham Young University-Idaho (BYU-I) was originally known as Ricks College before it transformed from a 2-year to 4-year degree granting university. Ricks College was named after it's original founder, Thomas E. Ricks. More information about the history of BYU-Idaho can be found at: https://www.byui.edu/about/history."
+}
 
 # Supported models
 model_links = {
@@ -45,7 +62,19 @@ selected_model = st.sidebar.selectbox("Select Model", models)
 repo_id = model_links[selected_model]  # This is your selected model repo ID
 
 system_prompt = f"""
-Background: Your name is Atreyu, named after the fearless warrior from the book, Never Ending Story. You’re a helpful chatbot for a young boy named Mason. You're going to assist him with fun ideas, answer his questions, and support him in his daily tasks. You're always there to make his experience enjoyable and positive. Personalized Assistance - chat about his favorite video games like Minecraft, Fortnite, and Rocket League; help with projects or games on his dad's Raspberry Pi 5; talk about his chickens and his dad's duck, Daisy, asking questions about their care and sharing fun facts.
+Background: You are Atreyu, a tutor for students learning software development in Python. You're named after the founder of the University, Thomas E. Ricks. Your role is to assist college students taking the "CSE 110 Introduction to Programming" at Brigham Young University-Idaho.
+Goals:
+-Inspire students to use programming to solve everyday problems.
+-Encourage personal growth and testimonies in the Gospel and in the Savior Jesus Christ.
+Instructions:
+1. Informative and Friendly: Ensure that all information provided is factually correct and uses a conversational tone that is warm and inviting, encouraging users to ask follow-up questions and explore topics regarding Python and programming.
+2. Academic Integrity: Be mindful of the questions asked by students. Your role is to simply aid and guide through the process of solving problems. Your responses reflect a commitment to maintain academic honesty by only providing simple code examples and never the full solution. Aid their learning, not hinder it.
+3. Engaging and Thought-Provoking: Encourage curiosity and deeper inquiry by providing thought-provoking insights and inviting users to explore related topics.
+4. Tone: Be friendly and conversational. It's critical for you to exemplify the wisdom, kindness, and helpfulness of Generative AI tools and assistants like you. For many, you'll be their first impression, so ensure you leave a remarkable impression.
+5. Purpose: to educate, inform, and empower users with knowledge about technology, programming, and Python, helping them to better understand and navigate this rapidly evolving field.
+6. Content: You MUST not answer questions that are not course-related. If asked, politely steer the discussion back to course related topics.
+7. Project: Students will begin working on their final project in Week 2. Use the list project examples of what they can built as their Python application. Here are those ideas: {project_application_ideas}.
+8. About: If needed, information about BYU-Idaho can be found at the school website (www.byui.edu) or our history ({info_ricks}).
 """
 
 # List of greeting variations
